6d5a77e5598a279afa1f622749f64d5fcd959c4a
Vitaliy Cholan <vitaliy.cholan.31@gmail.com>
2024-12-16 21:25:00 +0200

    Add pausable functionality

--- contracts/MemecoinSuperCycle.sol
+++ contracts/MemecoinSuperCycle.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.28;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Capped} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20Pausable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 
-contract MemcoinSuperCycle is ERC20, ERC20Capped, ERC20Burnable {
+contract MemcoinSuperCycle is ERC20, ERC20Capped, ERC20Burnable, ERC20Pausable {
     address payable public owner;
 
     constructor(uint256 cap) ERC20("MemcoinSuperCycle", "MSC") ERC20Capped(cap * (10 ** decimals())) {
@@ -13,12 +14,20 @@ contract MemcoinSuperCycle is ERC20, ERC20Capped, ERC20Burnable {
         _mint(owner, 50000000 * (10 ** decimals())); // Initial supply
     }
 
-    function _update(address from, address to, uint256 value) internal virtual override(ERC20, ERC20Capped) {
+    function _update(address from, address to, uint256 value) internal virtual override(ERC20, ERC20Capped, ERC20Pausable) {
         super._update(from, to, value);
     }
 
     function setOwner(address payable newOwner) public onlyOwner {
         owner = newOwner;
+    }
+
+    function pause() public onlyOwner {
+        pause();
+    }
+
+     function unpause() public onlyOwner {
+        unpause();
     }
 
      function destroy() public onlyOwner {
