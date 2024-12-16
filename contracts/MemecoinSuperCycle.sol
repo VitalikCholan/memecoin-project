2d49ad3d454fa61760adda30ffa590bfb9417631
Vitaliy Cholan <vitaliy.cholan.31@gmail.com>
2024-12-16 21:10:30 +0200

    Add owner function and self-destruct capability

--- contracts/MemecoinSuperCycle.sol
+++ contracts/MemecoinSuperCycle.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.28;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Capped} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract MemcoinSuperCycle is ERC20, ERC20Capped {
+contract MemcoinSuperCycle is ERC20, ERC20Capped, ERC20Burnable {
     address payable public owner;
 
     constructor(uint256 cap) ERC20("MemcoinSuperCycle", "MSC") ERC20Capped(cap * (10 ** decimals())) {
@@ -14,5 +15,18 @@ contract MemcoinSuperCycle is ERC20, ERC20Capped {
 
     function _update(address from, address to, uint256 value) internal virtual override(ERC20, ERC20Capped) {
         super._update(from, to, value);
+    }
+
+    function setOwner(address payable newOwner) public onlyOwner {
+        owner = newOwner;
+    }
+
+     function destroy() public onlyOwner {
+        payable(owner).transfer(address(this).balance); // Transfer remaining funds to owner.
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
     }
 }
