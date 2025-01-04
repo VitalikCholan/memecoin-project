1ec8e1e8a6bc77ea8204dd880e06d8eae577ffcd
Vitaliy Cholan <vitaliy.cholan.31@gmail.com>
2025-01-04 15:13:53 +0200

    Add routing in app

--- frontend/MemecoinSuperCycle/src/app/app.component.ts
+++ frontend/MemecoinSuperCycle/src/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
-import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
-
+import { RouterModule, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, AboutComponent],
+  imports: [RouterModule, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'MemecoinSuperCycle';
-}
+export class AppComponent {}
