8b976610681689fb591d757b739729a149cbc6cc
Vitaliy Cholan <vitaliy.cholan.31@gmail.com>
2025-01-04 12:25:07 +0200

    Develop HTML structure for About page

--- frontend/MemecoinSuperCycle/src/app/app.component.ts
+++ frontend/MemecoinSuperCycle/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [HomeComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
