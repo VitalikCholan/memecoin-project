7d58ac66656ea88b00b0b99c104cef8f49c4963b
Vitaliy Cholan <vitaliy.cholan.31@gmail.com>
2025-01-31 19:30:51 +0200

    Hide API keys

--- frontend/MemecoinSuperCycle/src/app/environments/environment.ts
+++ frontend/MemecoinSuperCycle/src/app/environments/environment.ts
@@ -1,7 +1,7 @@
+import { config } from '../config/config';
+
 export const environment = {
   production: false,
-  infuraSepolia:
-    'https://sepolia.infura.io/v3/eb5b4cba47fc45b09484a40db5fb9f18',
-  infuraBaseSepolia:
-    'https://base-sepolia.infura.io/v3/eb5b4cba47fc45b09484a40db5fb9f18',
+  infuraSepolia: `${config.infura.sepolia.endpoint}${config.infura.sepolia.key}`,
+  infuraBaseSepolia: `${config.infura.baseSepolia.endpoint}${config.infura.baseSepolia.key}`,
 };
--- frontend/MemecoinSuperCycle/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  infuraSepolia:
-    'https://sepolia.infura.io/v3/eb5b4cba47fc45b09484a40db5fb9f18',
-  infuraBaseSepolia:
-    'https://base-sepolia.infura.io/v3/eb5b4cba47fc45b09484a40db5fb9f18',
-};
